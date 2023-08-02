6a2fea158eee0dd43c6f912f4de3c0fa10b7001f
username <youremail@domain.com>
2023-08-02 18:19:01 -0400

    Initial Commit

---
+++ components/index.js
@@ -0,0 +1,214 @@
+/* eslint-disable */
+import React, { useEffect, useState } from "react";
+import { Button, Input } from "antd";
+import "antd/dist/antd.css";
+// import "font-awesome/css/font-awesome.min.css";
+import Header from "./Header";
+import Messages from "./Messages";
+import List from "./List";
+import socket from "socket.io-client";
+import {
+  ChatContainer,
+  StyledContainer,
+  ChatBox,
+  StyledButton,
+  SendIcon,
+} from "../pages/chat/styles";
+
+function ChatRoom({ username, id }) {
+  // States
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState("");
+  const [users, setUsers] = useState([]);
+  const [chatroom, setChatroom] = useState(`${id}`);
+  const [joinedChats, setJoinedChats] = useState([]);
+  const [chatName, setChatName] = useState("");
+  const [currentChatUsers, setCurrentChatUsers] = useState([]);
+
+  // Socket
+  const io = socket("http://localhost:1337");
+  let welcome;
+
+  // Effects
+  useEffect(() => {
+
+    io.on("disconnect", () => {
+      io.off();
+      location.replace("http://localhost:3000/");
+      console.log("disconnected");
+    });
+
+    io.emit("join", { username, chatroom }, (error) => {
+      if (error) return alert(error);
+    });
+
+    io.on("welcome", async (data, error) => {
+      let welcomeMessage = {
+        user: data.user,
+        message: data.text,
+      };
+      welcome = welcomeMessage;
+      setMessages([welcomeMessage]);
+
+    await fetch(`http://localhost:1337/api/chatrooms/${chatroom}?populate=messages,active_users`)
+      .then(async (res) => {
+        const response = await res.json();
+        let messagesArr = [welcome];
+        let usersArr = [];
+
+        // Get the chats messages
+        response.data.attributes.messages.data.forEach((message) => {
+          messagesArr = [...messagesArr, message.attributes];
+        });
+        // Get the chats users
+        response.data.attributes.active_users.data.forEach((user) => {
+          usersArr = [...usersArr, user.attributes];
+        });
+        
+        setMessages((msgs) => messagesArr);
+        setCurrentChatUsers((users) => usersArr);
+      })
+      .catch((e) => console.log(e.message));
+
+      await fetch(`http://localhost:1337/api/active-users/${id}?populate=chatrooms`)
+        .then(async (res) => {
+          const response = await res.json();
+      
+          let arr = response.data.attributes.chatrooms.data.map((chatroom) => chatroom);
+          setJoinedChats(arr);
+        })
+        .catch((e) => console.log(e.message));
+      
+    });
+
+    io.on("roomData", async (data) => {
+      await fetch(`http://localhost:1337/api/chatrooms/${chatroom}?populate=active_users`).then(async (e) => {
+        const response = await e.json();
+        let usersArr = [];
+
+        response.data.attributes.active_users.data.forEach((user) => {
+          usersArr = [...usersArr, user.attributes];
+        });
+        setCurrentChatUsers((users) => usersArr);
+      }); 
+    });
+
+    io.on("message", async (data, error) => {
+      await fetch(`http://localhost:1337/api/chatrooms/${chatroom}?populate=messages`)
+        .then(async (res) => {
+          const response = await res.json();
+          let arr = [welcome];
+
+          if (!response.data || !response.data.attributes || !response.data.attributes.messages || !response.data.attributes.messages.data) {
+            console.log("Response data or messages data is undefined:", response);
+            return;
+          }
+
+          response.data.attributes.messages.data.forEach((message) => {
+            arr = [...arr, message.attributes];
+          });
+
+          setMessages((msgs) => arr);
+        })
+        .catch((e) => console.log(e.message));
+    });
+  }, [username, chatroom]);
+
+  useEffect(() => {
+    console.log(currentChatUsers);
+  }, [currentChatUsers]);
+  
+
+
+  const sendMessage = (message, chatroom) => {
+    if (message) {
+      let strapiData = {
+        data: {
+          user: username,
+          message: message,
+          chatroom: chatroom,
+        },
+      };
+      io.emit("sendMessage", strapiData, (error) => {
+        if (error) {
+          alert(error);
+        }
+      });
+      setMessage("");
+    } else {
+      alert("Message can't be empty");
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setMessage(e.target.value);
+    setChatName(e.target.value);
+  };
+
+  const handleMessageSend = () => {
+    sendMessage(message, chatroom);
+  };
+
+  const handleChatCreate = async () => {
+    let strapiData = {
+      data: {
+        chat_name: chatName,
+        active_users: [{"id": 1}]
+      },
+    };
+    await fetch("http://localhost:1337/api/chatrooms", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(strapiData),
+    })
+      .then(async (e) => {
+        const response = await e.json();
+        setChatroom(response.data.id);
+      })
+      .catch((e) => console.log(e.message));
+  };
+
+  const handleChatChange = (chatroom) => {
+    setChatroom(chatroom);
+  };
+
+  return (
+    <ChatContainer>
+      <Header room="Group Chat" />
+      <StyledContainer>
+        <h3>Active Users in room: {chatroom}</h3>
+        <ul>
+          {currentChatUsers.map((user, index) => (
+            <li key={index}>{user.users}</li>
+          ))}
+        </ul>
+
+        <ChatBox>
+          <Messages messages={messages} username={username} />
+          <Input
+            type="text"
+            placeholder="Type your message"
+            value={message}
+            onChange={handleInputChange}
+          />
+          <StyledButton onClick={handleMessageSend}>
+            <SendIcon>
+              <i className="fa fa-paper-plane" />
+            </SendIcon>
+          </StyledButton>
+        </ChatBox>
+      </StyledContainer>
+      <h3>Joined Chats</h3>
+      <input onChange={handleInputChange} type="text" placeholder="Make a chat name" /><Button onClick={handleChatCreate}>Create Chat</Button>
+      <ul>
+        {joinedChats.map((chat) => (
+          <li onClick={() => handleChatChange(chat.id)} key={chat.id}>{chat.attributes.chat_name}</li>
+        ))}
+      </ul>
+    </ChatContainer>
+  );
+}
+
+export default ChatRoom;
